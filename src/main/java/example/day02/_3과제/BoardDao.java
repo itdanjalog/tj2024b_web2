657ba7651e5b6e1788b2981599e5e6e6a729df9c
itdanjalog <kgs2072@naver.com>
2025-02-18 17:01:49 +0900

    day67

--- src/main/java/example/day02/_3과제/BoardDao.java
+++ src/main/java/example/day02/_3과제/BoardDao.java
@@ -1,22 +1,22 @@
 package example.day02._3과제;
 
-import jakarta.servlet.http.HttpServlet;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController // (1) 해당 컨트롤러가 HTTP url 매핑 할 수 있도록 어노테이션 주입
-@RequestMapping("/day02/task1/board")
-public class BoardController extends HttpServlet {
+@Component
+public class BoardDao {
 
     // (2) 현재 과제에서는 DB가 없으므로 리스트가 DB 역할 한다.
     private final List<BoardDto> boardTable = new ArrayList<>();
     private int auto_increment = 1; // bno 자동번호 역할
 
     // (3) POST 매핑으로 '/day02/task1/board' 주소의 body값을 dto로 매핑하여 함수 매개변수로 받는다,
-    @PostMapping("") // 1. POST 매핑주소를 만든다.
-    public boolean method1( @RequestBody BoardDto boardDto ){ // 2.body 값을 함수 매개변수로 매핑한다.
+
+    public boolean method1(  BoardDto boardDto ){ // 2.body 값을 함수 매개변수로 매핑한다.
         boardDto.setBno( auto_increment ); // 3. boardDto의 bno를 넣어준다.
         boardTable.add( boardDto ); // 4. 리스트에 boardDto를 넣어준다.
         auto_increment++; // 5. auto_increment 증가한다.
@@ -24,14 +24,12 @@ public class BoardController extends HttpServlet {
     } // f end
 
     // (4) GET 매핑
-    @GetMapping("")
     public List<BoardDto> method2(){
         return boardTable;
     } // F END
 
     // (5) GET 매핑
-    @GetMapping("/view")
-    public BoardDto method3( @RequestParam( name = "bno") int bno ){
+    public BoardDto method3( int bno ){
         for( int index = 0 ; index <= boardTable.size()-1 ; index++ ){
             BoardDto boardDto = boardTable.get(index);
             if( boardDto.getBno() == bno ){ return boardDto; } // 찾았으면 찾은 DTO 반환
@@ -39,9 +37,7 @@ public class BoardController extends HttpServlet {
         return null; // 없으면
     } // F END
 
-    // (6) PUT 매핑
-    @PutMapping("")
-    public boolean method4( @RequestBody BoardDto boardDto ){
+    public boolean method4(  BoardDto boardDto ){
         for( int index = 0 ; index <= boardTable.size()-1 ; index++ ){
             BoardDto boardDto2 = boardTable.get( index );
             if( boardDto2.getBno() == boardDto.getBno() ){
@@ -52,9 +48,7 @@ public class BoardController extends HttpServlet {
         return false;
     } // f end
 
-    // (7) DELETE 매핑
-    @DeleteMapping("/day02/task1/board")
-    public boolean mehtod5( @RequestParam( name = "bno") int bno ){
+    public boolean mehtod5( int bno ){
         for( int index = 0 ; index <= boardTable.size()-1 ; index++  ){
             BoardDto boardDto2 = boardTable.get(index);
             if( boardDto2.getBno() == bno ){
@@ -65,4 +59,4 @@ public class BoardController extends HttpServlet {
         return false;
     } // f end
 
-} // c end
+}
