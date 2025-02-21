b765691352feacc28ac0690285c4bcfdd318751e
itdanjalog <kgs2072@naver.com>
2025-02-21 16:37:26 +0900

    day68

--- src/main/java/example/day03/mvc2_mybatis/AppStart.java
+++ src/main/java/example/day03/mvc2_mybatis/AppStart.java
@@ -4,4 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppStart { public static void main(String[] args) {SpringApplication.run( AppStart.class );} }
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run( AppStart.class );
+    }
+}
--- src/main/java/example/day03/mvc2_mybatis/model/mapper/MyMapper.java
+++ src/main/java/example/day03/mvc2_mybatis/model/mapper/MyMapper.java
@@ -5,8 +5,8 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MyMapper {
-    // [1] 등록 SQL 매퍼
-    int save( MyDto myDto ); // 추상메소드 매핑 구현
+    // [1] 등록 SQL 매퍼 추상메소드
+    int save( MyDto myDto );
 }
 
 
