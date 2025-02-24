c3fe24e1c79c9e8f0f5a83a0241eecb9aa463ca2
itdanjalog <kgs2072@naver.com>
2025-02-24 10:55:08 +0900

    day71

--- src/main/java/example/day03/_과제/model/mapper/BoardMapper.java
+++ src/main/java/example/day03/_과제/model/mapper/BoardMapper.java
@@ -3,9 +3,20 @@ package example.day03._과제.model.mapper;
 import example.day03._과제.model.dto.BoardDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface BoardMapper {
 
-    int save(BoardDto boardDto ); // [4] .xml 파일의 SQL 매핑
+    int save(BoardDto boardDto );// [4] .xml 파일의 SQL 매핑
 
+    public abstract List< BoardDto > findAll();
+
+    BoardDto find( int bno );
+
+
+    int update(  BoardDto boardDto );
+
+
+    int delete(  int bno );
 }
