94fcc7355aeff86310f79c2e1ab00e391455da23
itdanjalog <kgs2072@naver.com>
2025-02-27 12:22:40 +0900

    day74

--- src/main/java/example/day06/aop/Example2.java
+++ src/main/java/example/day06/aop/Example2.java
@@ -1,10 +1,8 @@
 package example.day06.aop;
 
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -36,18 +34,36 @@ class Security{
     
     // [3] 
     @Before("execution( * example.day06.aop.TestService2.enter3(..)) && args(name)")
-    // 지정한 메소드에서 name 매개변수를 가지고 와서 아래 메소드에서 사용이 가능하다.
-        // 로그처리, 보안검사 , 트랜잭션 등등 하기 위해서
+    // 지정한 메소드에서 name 매개변수를 가지고 와서 아래 메소드에서 사용이 가능하다. // 로그처리, 보안검사 , 트랜잭션 등등 하기 위해서
     public void securityCheck3( String name ){
         System.out.println("메소드 실행 전(before) : " + name +"님이 코로나 검사 완료");
     } // f end
 
     // [4]
-    @AfterReturning(value = "execution( * example.day06.aop.TestService2.enter3(..))" ,
-                returning = "result" )
+    @AfterReturning(value = " * execution( * example.day06.aop.TestService2.enter3(..))" , returning = "result" )
     // 지정한 메소드가 정상적으로 실행 되었을때 result 리턴값을 가지고 와서 아래 메소드에서 사용이 가능하다.
+        // 즉] 'TestService2' enter3 메소드가 실행후 return 값을 가지고 'securityCheck4' 실행
     public void securityCheck4( Object result ){
         System.out.println("메소드 실행 후(after) : " + result +" 이/가 결과값  정상 ");
+    }
+
+    // [5] 메소드 실행 시간 체크 , 검증
+    @Around(" execution( * example.day06.aop.TestService2.enter3(..) )" )
+    public Object timeCheck5( ProceedingJoinPoint joinPoint ) throws Throwable {
+        // ProceedingJoinPoint : 지정한 메소드 와 추가적인 로직과 조합해서 구현
+        // --> 추가 로직에서 직접 지정한 메소드 호출 : joinPoint.proceed(); , 예외처리 필수
+        // (1) 시작 시간
+        long startTime = System.currentTimeMillis(); // System.currentTimeMillis() : 현재 시간을 밀리초(1/1000초) 반환
+        // (*) // 지정된 메소드( 'enter3' ) 실행
+        Object result = joinPoint.proceed();
+        // (2) 끝나는 시간
+        long endTime = System.currentTimeMillis(); // 현재 시간을 밀리초(1/1000초) 반환
+        // (3) 지정된 메소드가 실행했을때 걸린시간 계산
+        long timeMs = endTime - startTime;
+        // (4) 걸린시간 출력
+        System.out.println("enter3 메소드가 실행된 시간 : " + timeMs + " ms");
+        // (5) 지정된 메소드가 리턴한 값 그대로 리턴
+        return result;
     }
 
 } // class end
