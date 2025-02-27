0bf4c75abdb4717d5649f1a74fd1d7442007decc
itdanjalog <kgs2072@naver.com>
2025-02-27 11:17:33 +0900

    day74

--- src/main/java/example/day06/aop/Example2.java
+++ src/main/java/example/day06/aop/Example2.java
@@ -1,6 +1,7 @@
 package example.day06.aop;
 
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Component
 class Security{
     @Before("execution( * TestService2.*(..) )")
+    // 지정한 메소드가 실행되기 전에 아래 메소드가 실행 된다.
+    // 지정한 메소드는 'TestService2' 클래스의 *(모든메소드) 이면서 (..)모든매개변수타입 *(모든리턴타입)
+        // 즉] 'TestService2' 모든 메소드들은 실행되기전에 'securityCheck()'메소드가 자동으로 실행된다.
     public void securityCheck(){
-        System.out.println("(보안) [코로나] 열  체크  + 자기진단 ");
+        System.out.println("메소드 실행 전(before) : (보안) [코로나] 열  체크  + 자기진단 ");
+    } // f end 
+    @After("execution( * example.day06.aop.TestService2.enter1(..) )")
+    public void securityCheck2(){
+        System.out.println("메소드 실행 후(after) : 서비스 종료 ");
     }
 } // class end
 
