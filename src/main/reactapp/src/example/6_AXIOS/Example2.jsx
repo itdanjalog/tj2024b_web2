972b5a964c45d3ecff4d1eb83f63ce273fd539ef
itdanjalog <kgs2072@naver.com>
2025-03-10 12:23:15 +0900

    day80

--- src/main/reactapp/src/example/6_AXIOS/Example2.jsx
+++ src/main/reactapp/src/example/6_AXIOS/Example2.jsx
@@ -27,9 +27,18 @@ export default function Example2( props ){
     }
 
     // [4] 서버에 저장된 방문록정보( 작성자/내용/비밀번호 여러개 ) 요청한다.
-    useEffect( () =>{ onFindAll()  } , [] ); // (1) 컴포넌트 마운트(생성) 될때 최초 1번 실행 함수 // 컴포넌트가 생성될때 딱 1번 onFindAll 함수가 실행된다.
+    
+    // (1) 컴포넌트 마운트(생성) 될때 최초 1번 실행 함수 // 컴포넌트가 생성될때 딱 1번 onFindAll 함수가 실행된다.
+    useEffect( () =>{ onFindAll()  } , [] ); // onFindAll --> setBoards 끝 
+    // useEffect( () =>{ onFindAll()  }  ); // onFindAll -->setBoards --> onFindAll --> setBoards 무한 렌더링 
+    // useEffect( () => { onFindAll } , [ boards ] ); // onFindAll --> setBoards -->  onFindAll --> setBoards 무한 렌더링 
+    
     const [ boards , setBoards ] = useState([]); //(2) 여러개의 방문록 가지는 state 변수 
+
     const onFindAll =  async ( e ) => {
+        // (3) 주로 동기 통신 : async( e ) => { await axios.메소드명() }
+            // -> 서버와 도메인 같을경우 : '/day07/react'
+            // -> 서버와 도메인 다른경우 : http://localhost:8080/day07/react
         try{const response = await axios.get('http://localhost:8080/day07/react');
             setBoards( response.data ); // 서버로 부터 받은 정보를 state 변수에 저장 // 재렌더링 
         }catch(error){ console.log( error ); }
@@ -44,12 +53,12 @@ export default function Example2( props ){
                 <button type="button" onClick={ onPost }> 등록 </button>
             </form>
             <table>
-                <thead> <tr> <th> 번호 </th> <th> 작성자 </th> <th> 방문록내용 </th> <th> 비고 </th>  </tr></thead>
+                <thead><tr><th>번호</th><th>작성자</th><th>방문록내용</th><th>비고</th></tr></thead>
                 <tbody>
                     {
                         boards.map( ( board , index ) => {
                             return ( 
-                                <tr> 
+                                <tr key={ board.num }> 
                                     <td> { board.num }</td> 
                                     <td> { board.writer}</td> 
                                     <td> { board.content}</td> 
