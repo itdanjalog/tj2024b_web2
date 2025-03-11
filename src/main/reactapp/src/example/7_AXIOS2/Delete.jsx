704d5b2c204ab2c68aa8fa7f0e1b630511ff583d
itdanjalog <kgs2072@naver.com>
2025-03-11 17:55:34 +0900

    day81

--- src/main/reactapp/src/example/7_AXIOS2/Delete.jsx
+++ src/main/reactapp/src/example/7_AXIOS2/Delete.jsx
@@ -1,3 +1,20 @@
+import axios from "axios"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { BarChart } from '@mui/x-charts/BarChart';
+
 export default function Delete( props ){
-    return(<> <div> Delete 페이지 </div> </>)
-}
+    return (
+        <BarChart
+          series={[
+            { data: [35, 44, 24, 34] },
+            { data: [51, 6, 49, 30] },
+            { data: [15, 25, 30, 50] },
+            { data: [60, 50, 15, 25] },
+          ]}
+          height={290}
+          xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
+          margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+        />
+      );
+    }
--- src/main/reactapp/src/example/7_AXIOS2/Read.jsx
+++ src/main/reactapp/src/example/7_AXIOS2/Read.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from 'axios';
 
 export default function Read( props ){
@@ -13,11 +13,32 @@ export default function Read( props ){
     const onRead = async ( ) =>{
         const response = await axios.get('http://localhost:8080/day08/products')
         console.log( response.data );
+        setProducts( response.data ); // 서버로부터 받은 모든 제품정보를 상태변수에 저장한다.
     }
+    // [3] 서버로 부터 받은 결과를 저장하는 상태 변수 
+    const [ products , setProducts ]  = useState( [] )
+    console.log( products )
 
     return(<> 
         <div> 
             <h3> Read 페이지 </h3> 
+            <table>
+                <thead>
+                    <tr> <th> 번호 </th> <th>제품명</th><th>제품설명</th><th>제품가격</th></tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map( ( product , index ) => { 
+                            return ( <tr>
+                                <td> { product.id }</td>
+                                <td> { product.name }</td>
+                                <td> { product.comment }</td>
+                                <td> { product.price }</td>
+                            </tr>)
+                        })
+                    }
+                </tbody>
+            </table>
         </div>
     </>)
 }
