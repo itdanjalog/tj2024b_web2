95d8ef116a1413e00d82390fa959f4fce455ae94
itdanjalog <kgs2072@naver.com>
2025-03-13 13:02:52 +0900

    day81

--- src/main/reactapp/src/main.jsx
+++ src/main/reactapp/src/main.jsx
@@ -71,5 +71,12 @@ const root = createRoot( document.querySelector('#root') )
 // root.render( <Example2/> )
 
 // day06 : 라우터 컴포넌트 렌더링
-import App from './example/7_AXIOS2/App'
-root.render( <App /> )
+// import App from './example/7_AXIOS2/App'
+// root.render( <App /> )
+
+// day07 : 
+// import Example1 from './example/8_MUI/Example1'
+// root.render( <Example1 /> )
+
+import Example2 from './example/8_MUI/Example2'
+root.render( <Example2 /> )
