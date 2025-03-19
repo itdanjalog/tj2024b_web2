e3f6a562b55a56f3e286ac468ecb4acecf242662
itdanjalog <kgs2072@naver.com>
2025-03-19 14:06:23 +0900

    day87

--- src/main/reactapp/src/components/App.jsx
+++ src/main/reactapp/src/components/App.jsx
@@ -6,23 +6,24 @@ import Header from './layouts/Header'
 import Footer from './layouts/Footer'
 import Signup from './member/Signup'
 import Login from './member/Login'
+import store from './reduxs/store'
+
+import { Provider } from '@reduxjs/toolkit'
  
 export default function App( props ){
     return (<>
-        <BrowserRouter> {/* 모든 라우팅 감싼다. */}
-            <div id="wrap">
-
-                <Header /> { /* Routes 밖에 있는 컴포넌트들이 고정  */}
-
-                <Routes> { /* Routes 안에 있는 컴포넌트들이 전환 */}
-                    <Route path='/' element={ <Home/> }> </Route>
-                    <Route path='/member/signup' element={ <Signup/> }> </Route>
-                    <Route path='/member/login' element={ <Login/> }> </Route>
-                </Routes>
-
-                <Footer /> { /* Routes 밖에 있는 컴포넌트들이 고정  */}
-
-            </div>
-        </BrowserRouter>
+        <Provider store = { store }> { /* 리덕스 스토어 적용 : 현재 Provider 로 감싼 컴포넌트 모두 리덕스 전역상태를 사용할 수 있다.  */ }
+            <BrowserRouter> {/* 모든 라우팅 감싼다. */}
+                <div id="wrap">
+                    <Header /> { /* Routes 밖에 있는 컴포넌트들이 고정  */}
+                    <Routes> { /* Routes 안에 있는 컴포넌트들이 전환 */}
+                        <Route path='/' element={ <Home/> }> </Route>
+                        <Route path='/member/signup' element={ <Signup/> }> </Route>
+                        <Route path='/member/login' element={ <Login/> }> </Route>
+                    </Routes>
+                    <Footer /> { /* Routes 밖에 있는 컴포넌트들이 고정  */}
+                </div>
+            </BrowserRouter>
+        </Provider>
     </>)
 }
--- src/main/reactapp/src/components/member/Login.jsx
+++ src/main/reactapp/src/components/member/Login.jsx
@@ -2,7 +2,14 @@ import { useState } from "react"
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
+import { useDispatch } from '@reduxjs/toolkit'
+import { login } from '../reduxs/userSlice'
+
 export default function Login( props ){
+
+    // (*****************************) 리덕스 ( 전역변수 ) 사용하기. (*****************************) 
+    // (1) 리덕스 사용하기 위한 useDispatch 함수 가져오기 
+    const dispatch = useDispatch();
 
     // (1) 입력받은 값들을 저장하는 state 변수
     const [ memberInfo , setMemberInfo ] = useState({ mid:'' , mpwd : ''} )
@@ -11,14 +18,20 @@ export default function Login( props ){
     // (3) 입력받은 값들을 axios 이용하여 자바(스프링) 에게 요청과 응답받기
     const navigate = useNavigate(); 
     const onLogin = async () => {
-        const response = await axios.post('http://localhost:8080/api/member/login' , memberInfo )
+        const response = await axios.post('http://localhost:8080/api/member/login' , memberInfo , { withCredentials : true } )
         const result = response.data;
         if( result == true ){ 
             alert('로그인성공'); 
-            navigate("/"); // navigate("/") : 새로고침없다 vs location.href="/": 새로고침있다.
-         }
+            // navigate("/"); // navigate("/") : 새로고침없다 vs location.href="/": 새로고침있다.
+            // location.href="/";
+            // (*****************************) 리덕스 ( 전역변수 ) 사용하기. (*****************************) 
+            // (2) useDispatch 함수를 이용한 리듀서 함수 액션하기. // 로그인 액션(함수) 에 (로그인한)회원정보를 대입한다. // 전역변수 대입 
+            dispatch( login( response.data ) ); // useState(지역상태) 가 아닌 store(전역상태) 에 저장된다. 
+        }
         else{ alert('로그인실패'); }
     } // f end 
+
+    console.log( memberInfo );
 
     return (<> 
         <h3> 로그인페이지 </h3>
