9d0896ce873f28b0b78abfbef70c12d9599d6187
itdanjalog <kgs2072@naver.com>
2025-03-24 11:28:04 +0900

    day90

--- src/main/java/example/day10/_멀티스레드/Example1.java
+++ src/main/java/example/day10/_멀티스레드/Example1.java
@@ -28,12 +28,12 @@ public class Example1 {
             try{ Thread.sleep( 1000 );}
             catch ( Exception e ){  System.out.println( e ); }
         }
-        // 4. ================================== 멀티 스레드 ================================== //
+        // 4. ================================== 멀티 스레드 (1) 익명 객체 ================================== //
         System.out.println("========================= 멀티 스레드 시작 ======================== ");
         // 4-1 : 소리 5번 발생 , 작업스레드 정의
-        Thread thread = new Thread(new Runnable() {
+        Thread thread = new Thread( new Runnable() {
             @Override
-            public void run() { // 추상메소드
+            public void run() { // 추상메소드 // 오버라이딩 // 재정의
                 Toolkit toolkit = Toolkit.getDefaultToolkit();
                 for( int i = 1 ; i <= 5 ; i++ ){
                     toolkit.beep(); // 비프음
@@ -51,5 +51,48 @@ public class Example1 {
             catch ( Exception e ){  System.out.println( e ); }
         }
 
+        // 5. ================================== 멀티 스레드 (2) 구현 객체 ================================== //
+        System.out.println("========================= 멀티 스레드 시작 ======================== ");
+        // 5-1 : 소리 5번 발생 , 작업스레드 정의
+        비프음1 비프음1 = new 비프음1();
+        Thread 비프음스레드1 = new Thread( 비프음1 );
+        비프음스레드1.start();
+        // 5-2 : 출력 5번 발생 , (위 예제 코드 복사해오기.)
+        for( int i = 1 ; i <= 5 ; i++ ){
+            System.out.println("띵");
+            try{ Thread.sleep( 1000 );}
+            catch ( Exception e ){  System.out.println( e ); }
+        }
+
+        // 6. ================================== 멀티 스레드 (3) 상속 객체 ================================== //
+        System.out.println("========================= 멀티 스레드 시작 ======================== ");
+        // 6-1 : 소리 5번 발생 , 작업스레드 정의
+        비프음2 비프음2 = new 비프음2();
+        비프음2.start();
+
+        // 6-2 : 출력 5번 발생 , (위 예제 코드 복사해오기.)
+        for( int i = 1 ; i <= 5 ; i++ ){
+            System.out.println("띵");
+            try{ Thread.sleep( 1000 );}
+            catch ( Exception e ){  System.out.println( e ); }
+        }
     } // m end
 } // c end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
