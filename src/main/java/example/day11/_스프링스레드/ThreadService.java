d9f9b39a72b2d858fd70c4e37d4b6f4dad040b8d
itdanjalog <kgs2072@naver.com>
2025-03-25 11:41:34 +0900

    day91

--- src/main/java/example/day11/_스프링스레드/ThreadService.java
+++ src/main/java/example/day11/_스프링스레드/ThreadService.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 @Service // 스프링MVC 에서 Service
 public class ThreadService {
-    // (1) 비동기 , 스레드풀 자동 적용
+    // (1) 비동기 , +스레드풀 자동 적용
     @Async // [2] 멀티스레드를 적용할 메소드 위에 @Async
     public void thread1(){
         String name = UUID.randomUUID().toString() ; // 구분용
@@ -17,7 +17,7 @@ public class ThreadService {
             catch (Exception e){ System.out.println(e);}
         }
     }
-    // (2) 동기 , synchronized
+    // (2) 동기 , synchronized  , +스레드풀 자동 적용
     @Async
     public synchronized void thread2(){
         //위 예시 코드 복사
